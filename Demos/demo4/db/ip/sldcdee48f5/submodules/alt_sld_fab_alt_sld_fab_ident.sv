bd2404e747ebf655675775acf2b5a1b8d3a13c5b
Jackson Charnock (RIT Student) <eec9960@rit.edu>
2025-02-19 18:43:16 -0500

    Finished signal tap demo will start prelab for lab 5.

--- Demos/demo4/db/ip/sldcdee48f5/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ Demos/demo4/db/ip/sldcdee48f5/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 8fb6338b2bf5770da05a
+//   DESIGN_HASH   : ba7c39e1bf2b6cf21362
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h8fb6338b2bf5770da05a };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'hba7c39e1bf2b6cf21362 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
